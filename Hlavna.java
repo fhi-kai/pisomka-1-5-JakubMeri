b580b606a7353f41715b1b58a63721291265f604
Jakub Méri <36486652+JakubMeri@users.noreply.github.com>
2018-03-07 19:45:50 +0100

    Add files via upload

---
+++ Hlavna.java
@@ -0,0 +1,21 @@
+public  class Hlavna implements ZajacieUsi {
+
+
+@Override
+    public int zajacieUsi(int pocetZajacov)
+    {
+
+        if (pocetZajacov == 0)
+        {
+            return 0;
+        }
+        else if (pocetZajacov % 2 == 0)
+        {
+            return 3 + zajacieUsi(pocetZajacov - 1);
+        }
+        else
+        {
+            return 2 + zajacieUsi(pocetZajacov - 1);
+        }
+    }
+   }
